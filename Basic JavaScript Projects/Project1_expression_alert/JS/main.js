82b8c7cfd8f1f416ef60a8a93d830514bed13b3b
MonzurSiddique <mhsiddique@gmail.com>
2023-07-04 12:01:21 -0700

    commenting assignment

--- Basic JavaScript Projects/Project1_expression_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expression_alert/JS/main.js
@@ -1,5 +1,5 @@
-function My_First_Function() {
-    var str ="this is the button text!";
-    document.getElementById("Button_Text").innerHTML = str;
-   
+function My_First_Function() {                                 //Defining a function and naming it
+    var str ="this is the button text!";                       //Defining a variable and naming it
+                                                               //a string value
+    document.getElementById("Button_Text").innerHTML = str;    //Putting the value of the variable into the html elementFromPoint with the "Button_text" id   
 }
