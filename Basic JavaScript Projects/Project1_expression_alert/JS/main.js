ca94408ed64bc64b2fcd7a58fd3145ea356e6c6d
MonzurSiddique <mhsiddique@gmail.com>
2023-07-04 12:42:27 -0700

    Basic Java script project assignment

--- Basic JavaScript Projects/Project1_expression_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expression_alert/JS/main.js
@@ -1,5 +1,17 @@
-function My_First_Function() {                                 //Defining a function and naming it
-    var str ="this is the button text!";                       //Defining a variable and naming it
-                                                               //a string value
-    document.getElementById("Button_Text").innerHTML = str;    //Putting the value of the variable into the html elementFromPoint with the "Button_text" id   
-}
+//main.js
+
+//window.alert method
+window.alert("Hello, world!");
+
+//document.alert method
+document.alert("For the first time - Hello, world!");t
+
+//creating two variables
+var Sent1 = "This is the beginning of the string";
+var Sent1 = " and this is the end of the string";
+
+//Displaying the variables using the document.write method
+document.write(Sent1 + Sent2);
+
+//Example of an expression
+document.write(21/3);
